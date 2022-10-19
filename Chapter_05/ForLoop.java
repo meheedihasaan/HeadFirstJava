2def093bc407c2230171e47e4bfc9a892febad66
Mehedi Hasan <105338310+meheedihasaan@users.noreply.github.com>
2022-10-19 23:45:50 +0600

    Add files via upload

---
+++ Chapter_05/ForLoop.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter_05;
+
+/**
+ *
+ * @author Acer
+ */
+public class ForLoop {
+    
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
---
+++ Chapter_05/WhileLoop.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter_05;
+
+/**
+ *
+ * @author Acer
+ */
+public class WhileLoop {
+    
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 4, 8, 9};
+        int i = 0;
+        while(i < arr.length){
+            System.out.println(arr[i]);
+            i++;
+        }
+    }
+    
+}
