2b79e83531d06872a8dbdeb1e9ec82776c976e70
Mehedi Hasan <105338310+meheedihasaan@users.noreply.github.com>
2022-10-19 23:47:18 +0600

    Add files via upload

---
+++ Chapter_05/EnhancedForLoop.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter_05;
+
+/**
+ *
+ * @author Acer
+ */
+public class EnhancedForLoop {
+    
+    public static void main(String[] args) {
+        String cities[] = {"Dhaka", "Barisal", "Sylhet", "Rajshahi", "Rangpur"};
+        for (String city : cities) {
+            System.out.println(city);
+        }
+    }
+    
+}
