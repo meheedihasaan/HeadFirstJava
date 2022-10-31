028f1e8beaeac06df80f9581ff36f0d8f219f852
Mehedi Hasan <105338310+meheedihasaan@users.noreply.github.com>
2022-10-31 10:23:32 -0700

    Update AutoBoxingAndUnboxing_1.java

--- Chapter_10/AutoBoxingAndUnboxing_1.java
+++ Chapter_10/AutoBoxingAndUnboxing_1.java
@@ -11,19 +11,19 @@ package Chapter_10;
 public class AutoBoxingAndUnboxing_1 {
     
     public static void main(String[] args) {
-        //Autoboxing: Primitive int to Reference Integer
+        //Autoboxing: Primitive int to Wrapper Integer Class
         Integer a = new Integer(10);    //wrapped
-        //Unboxing: Reference Integer to Primitive int
+        //Unboxing: Wrapper Integer class to Primitive int
         System.out.println(a.intValue()); //unwrapped
         
-        //Autoboxing: Primitive double to Reference Double
+        //Autoboxing: Primitive double to Wrapper Double Class
         Double b = new Double(15.67);
-        //Unboxing: Reference Double to Primitive double
+        //Unboxing: Wrapper Double Class to Primitive double
         System.out.println(b.doubleValue());
         
-        //Autoboxing: Privitive char to Character
+        //Autoboxing: Privitive char to Wrapper Character Class
         Character c = new Character('m');
-        //Unboxing: Reference Character to Primitive char
+        //Unboxing: Wrapper Character Class to Primitive char
         System.out.println(c.charValue());
     }
     
