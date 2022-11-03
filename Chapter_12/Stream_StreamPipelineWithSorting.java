3e0a94187c2b6d96d09d47a59de24623fd334e63
Mehedi Hasan <105338310+meheedihasaan@users.noreply.github.com>
2022-11-03 21:50:59 +0600

    Add files via upload

---
+++ Chapter_12/Stream_StreamPipelineWithSorting.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter_12;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author User
+ */
+public class Stream_StreamPipelineWithSorting {
+    public static void main(String[] args) {
+        List<String> songList = List.of("Faded", "Sorry", "Alone", "Lilly");
+        
+        //Stream Pipeline
+        List<String> resultList = songList.stream() //Creating Stream
+                                        .sorted() //Sorting Stream
+                                        .limit(3) //Taking three songs
+                                        .collect(Collectors.toList()); //Collect songs into resultList
+        System.out.println(resultList);
+    }
+}
